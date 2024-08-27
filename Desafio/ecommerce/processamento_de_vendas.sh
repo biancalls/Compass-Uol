b314cac96cb734ac56a0974a71622f388ecb8b40
biancalls <bianca.lages@icloud.com>
2024-08-27 19:18:28 -0300

    resolucao sh

--- Desafio/ecommerce/processamento_de_vendas.sh
+++ Desafio/ecommerce/processamento_de_vendas.sh
@@ -1,28 +1,26 @@
-#!/Downloads/Sprint_1/Sprint_1/Desafio/ecommerce
+cd /home/biancalages/Downloads/Sprint_1/Sprint_1/Desafio/ecommerce
 mkdir vendas
 cp dados_de_vendas.csv vendas
-cd ./vendas/
+cd vendas
 mkdir backup
-hoje=$(date + "%Y%M%D")
-periodo=$(date + "%Y%M%D %H:%M")
-mv dados_de_vendas.csv dados-$hoje.csv
-cp dados-$hoje.csv backup
-cd ./backup/
-mv dados-$hoje.csv backup-dados-$hoje.csv
-cd ./vendas/
+hoje=$(date +"%Y%m%d")
+periodo=$(date +"%Y%m%d %H:%M")
+cp dados_de_vendas.csv dados-$hoje.csv
+cp dados-$hoje.csv backup/backup-dados-$hoje.csv
+cd backup
 touch relatorios-$hoje.txt
-primeira_vendas=$(head -n 2 backup-dados-$hoje.csv)
-ultima_venda=$(tail -n 1 backup-dados-$hoje.csv )
-itens_vendidos=$(cut -d ','-f2 backup-dados-$hoje.csv |sort |uniq -c)
-dez_primeiras=$(head -f 10 backup-dados-$hoje.csv)
+primeira_vendas=$(head -n 2 backup-dados-$hoje.csv | cut -d ',' -f 5)
+ultima_venda=$(tail -n 1 backup-dados-$hoje.csv | cut -d ',' -f 5)
+itens_vendidos=$(tail -n +2 backup-dados-$hoje.csv |cut -d ',' -f 2 |sort |uniq | wc -l)
+dez_primeiras=$(head backup-dados-$hoje.csv)
 echo "Periodo: $periodo" >> relatorios-$hoje.txt
-echo "Primeira venda: $primeira_venda" >> relatorios-$hoje.txt
+echo "Primeira venda: $primeira_vendas" >> relatorios-$hoje.txt
 echo "Ultima venda: $ultima_venda" >> relatorios-$hoje.txt
 echo "Itens Vendidos: $itens_vendidos" >> relatorios-$hoje.txt
 echo "Dez Primeiras vendas: $dez_primeiras" >> relatorios-$hoje.txt
 zip -r backup-dados-$hoje.zip backup-dados-$hoje.csv
-rm dados_de_vendas.csv
-cd ./backup
 rm backup-dados-$hoje.csv
+cd ../
+rm dados_de_vendas.csv
 
 
