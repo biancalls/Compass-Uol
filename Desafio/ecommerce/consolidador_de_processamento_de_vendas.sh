e9b6e11e6fa4a234c62bbca2a9a50669991bea94
biancalls <bianca.lages@icloud.com>
2024-08-29 11:39:08 -0300

    consolidador sh draft

---
+++ Desafio/ecommerce/consolidador_de_processamento_de_vendas.sh
@@ -0,0 +1,8 @@
+cd ./home/biancalages/Downloads/Sprint_1/Git_Linux/Desafio/ecommerce/vendas/backup
+dia28=$("relatorios-20240828.txt")
+dia29=$("")
+dia30=$("")
+dia31=$("")
+cat "$dia28" "$dia29" "$dia30" "$dia31" > consolidador.txt
+awk "{ print $dia28, $dia29, $dia30, $dia31 }" consolidador.txt >> relatorio_final.txt
+cp relatorio_final.txt ecommerce
