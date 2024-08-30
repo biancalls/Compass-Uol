ccbccfb5dc2db025f9cd4490210cc335b17c1cea
biancalls <bianca.lages@icloud.com>
2024-08-30 15:35:06 -0300

    crontab dia3

--- Desafio/ecommerce/consolidador_de_processamento_de_vendas.sh
+++ Desafio/ecommerce/consolidador_de_processamento_de_vendas.sh
@@ -1,7 +1,7 @@
 cd ./home/biancalages/Downloads/Sprint_1/Git_Linux/Desafio/ecommerce/vendas/backup
 dia28=$("relatorios-20240828.txt")
 dia29=$("relatorios-20240829.txt")
-dia30=$("")
+dia30=$("relatorios-20240830.txt")
 dia31=$("")
 cat "$dia28" "$dia29" "$dia30" "$dia31" > consolidador.txt
 awk "{ print $dia28, $dia29, $dia30, $dia31 }" consolidador.txt >> relatorio_final.txt
